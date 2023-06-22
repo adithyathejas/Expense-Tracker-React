4776847f0be8e74ea69a9047366a734969cf795e
adithyathejas <adithyathejus00@gmail.com>
2023-06-22 21:18:53 +0530

    email verification implemented

--- src/Pages/ExpenseTracker.js
+++ src/Pages/ExpenseTracker.js
@@ -1,7 +1,17 @@
+import axios from 'axios'
 import {Button,Badge,Row,Col, Container} from 'react-bootstrap'
 import { useNavigate } from 'react-router-dom'
 
 const ExpenseTracker= ()=> {
+    const emailVerificationHandler=async ()=>{
+        try{
+        let token = localStorage.getItem('token')
+        let res = await axios.post('https://identitytoolkit.googleapis.com/v1/accounts:sendOobCode?key=AIzaSyACEG0j7ZsoZfGZzWq5No8W8_75yFKPx-k',{requestType:"VERIFY_EMAIL",idToken:token})
+        alert(`verification mail sent to ${res.data.email} `)
+        }catch(e){
+           alert(e.response.data.error.message)
+        }
+    }
     const Navigate =useNavigate()
     return(
         <>
@@ -14,6 +24,9 @@ const ExpenseTracker= ()=> {
         <Badge bg='secondary'>your profile is incomplete. <Button className='m-3' onClick={()=>{
             Navigate('/profile')
         }}>complete now</Button></Badge>
+        </Col>
+        <Col xs='2'>
+        <Badge bg='secondary'>verify email<Button className='m-3' onClick={emailVerificationHandler}>verify now</Button></Badge>
         </Col>
         </Row>
         </Container>
